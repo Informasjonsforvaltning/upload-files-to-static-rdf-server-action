f438e0d278a61f19b7421727f02b7722dd72204d
Amund Tenstad <github@amund.io>
2022-09-21 14:38:30 +0200

    feat: custom target filename

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,12 +11,18 @@ do
     file=$(echo $file_args | cut -d " " -f1)
     file_type=$(echo $file_args | cut -d " " -f2)
     file_language=$(echo $file_args | cut -d " " -f3)
+    file_name=$(echo $file_args | cut -d " " -f4)
     if [[ "$file" != "" ]] && [[ "$file_type" != "" ]] && [[ "$file_language" != "" ]]
     then
+        filename_arg=""
+        if [[ "$file_name" != "" ]]
+        then
+            filename_arg="filename=$file_name;"
+        fi
         echo "Uploading $file with type $file_type and content-language $file_language to folder $ontology_type at $url  "
         status_code=$(curl --write-out '%{http_code}' --silent --output /dev/null \
         -H "X-API-KEY: $api_key" \
-        -F "ontology-file=@$file;type=$file_type;headers=\"content-language: $file_language\"" \
+        -F "ontology-file=@$file;type=$file_type;${filename_arg}headers=\"content-language: $file_language\"" \
         -X PUT $url)
         if [[ $status_code -eq 201 ]]
         then
